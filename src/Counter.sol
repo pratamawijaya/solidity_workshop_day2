d1dae15ce8adaee5d337f0b936281406d6d55773
pratamawijaya <set.mnemonix@gmail.com>
2025-01-25 11:01:40 +0700

    Fix fmt check

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     // storage
-    uint256 public number;  // storage index 0
+    uint256 public number; // storage index 0
     uint256 public price; // storage index 1
 
     // contract owner
     address public owner;
-  
+
     constructor() {
         // set owner to contract creator
         owner = msg.sender;
     }
 
     /**
-    * @notice Sets a new value for the number.
-    *
-    * This function allows anyone to set a new value for the `number` variable.
-    *
-    * @param newNumber The new value that will be set as the `number`.
-    */ 
+     * @notice Sets a new value for the number.
+     *
+     * This function allows anyone to set a new value for the `number` variable.
+     *
+     * @param newNumber The new value that will be set as the `number`.
+     */
     function setNumber(uint256 newNumber) public {
         number = newNumber;
     }
